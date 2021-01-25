3c80621f99dd5f1c7d8c771ad2092495c3535dd6
Fellipe <fellipe_emanoel@univem.edu.br>
2021-01-24 22:46:43 -0300

    Estilizando o dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,10 +1,56 @@
 import React from 'react';
+import {FiChevronRight} from 'react-icons/fi';
 
-import {Title} from './styles';
+import logoImage from '../../assets/logo.svg';
+
+import {Title, Form, Repositories} from './styles';
 
 const Dashboard:React.FC = () => {
     return(
-        <Title>Explore repositórios no Github.</Title>
+        <>
+            <img alt="logoImage"src={logoImage}/>
+            <Title>Explore repositórios no Github.</Title>
+
+            <Form action="">
+                <input placeholder="Digite o nome do repositório"/>
+                <button>Pesquisar</button>
+            </Form>
+
+            <Repositories>
+                <a href="teste">
+                    <img src="https://avatars.githubusercontent.com/u/48105194?s=460&u=6ebc9d607602161c75c2e9d4f22eb1cc80b3ce19&v=4" alt="avatar"/>
+                    <div>
+                        <strong>Teste React</strong>
+                        <p>Testes do React JS </p>
+                    </div>
+                    <FiChevronRight size={20}/>
+                </a>
+                <a href="teste">
+                    <img src="https://avatars.githubusercontent.com/u/48105194?s=460&u=6ebc9d607602161c75c2e9d4f22eb1cc80b3ce19&v=4" alt="avatar"/>
+                    <div>
+                        <strong>Teste React</strong>
+                        <p>Testes do React JS </p>
+                    </div>
+                    <FiChevronRight size={20}/>
+                </a>
+                <a href="teste">
+                    <img src="https://avatars.githubusercontent.com/u/48105194?s=460&u=6ebc9d607602161c75c2e9d4f22eb1cc80b3ce19&v=4" alt="avatar"/>
+                    <div>
+                        <strong>Teste React</strong>
+                        <p>Testes do React JS </p>
+                    </div>
+                    <FiChevronRight size={20}/>
+                </a>
+                <a href="teste">
+                    <img src="https://avatars.githubusercontent.com/u/48105194?s=460&u=6ebc9d607602161c75c2e9d4f22eb1cc80b3ce19&v=4" alt="avatar"/>
+                    <div>
+                        <strong>Teste React</strong>
+                        <p>Testes do React JS </p>
+                    </div>
+                    <FiChevronRight size={20}/>
+                </a>
+            </Repositories>
+        </>
     )
 }
 
