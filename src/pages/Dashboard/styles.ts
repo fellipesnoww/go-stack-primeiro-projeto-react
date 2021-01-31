69685e5cd519f96ea6d3f49da966cdab871b3d3b
Fellipe <fellipe_emanoel@univem.edu.br>
2021-01-30 22:57:06 -0300

    Conectando com a api do Git

--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -71,7 +71,8 @@ export const Repositories = styled.div`
         }
 
         div{
-            margin-left: 16px;
+            margin: 0 16px;
+            flex: 1;
             font-family: Roboto;
 
             strong{
